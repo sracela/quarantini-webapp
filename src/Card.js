53cd6737da966e6644fa52650c9124faa19bebd2
sracela <saracelalfonso@gmail.com>
2020-05-21 04:05:54 +0200

    Style everything in the Tinder look and feel

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import style from './card.module.css';
+import last_image from "./covid1.jpg";
 
-const Card = ({description,image_url}) => {
-    return(
-        <div className={style.card}>
-            <div className={style.imageDiv}>
-                <img className={style.image} src={image_url} alt=""/>
+
+const Card = ({description, image_url, onTest=0, last_id=0, value=0}) => {
+    const last_description = "No more cards, suggest more sending an email to quarantini@gmail.com";
+    const first_description = "Loading cards";
+    console.log(value);
+    // console.log(last_id);
+
+    if(!last_id){
+        return(
+            <div className={style.card}>
+                <div className={style.imageDiv}>
+                    <img className={style.image} src={last_image} alt=""/>
+                </div>
+                <div className={style.textDiv}><p>{first_description}</p></div>
             </div>
-            <div className={style.textDiv}><p>{description}</p></div>
-        </div>
-    );
+        );
+      }
+  
+      if(value === last_id){
+        return(
+            // <div className={style.card}  onClick={() => onTest()}>
+            <div className={style.card} >
+                <div className={style.imageDiv}>
+                    <img className={style.image} src={last_image} alt=""/>
+                </div>
+                <div className={style.textDiv}><p>{last_description}</p></div>
+                {/* <PlusButton onClick={() => onTest()}/> */}
+            </div>
+        );
+      } else{
+
+        return(
+            <div className={style.card}>
+                <div className={style.imageDiv}>
+                    <img className={style.image} src={image_url} alt=""/>
+                </div>
+                <div className={style.textDiv}><p>{description}</p></div>
+            </div>
+        );
+
+      }
+
+
 }
 
 export default Card;
--- src/Game.js
+++ src/Game.js
@@ -1,93 +1,71 @@
 import React from 'react';
 import Card from "./Card";
-import './Game.css'
+
 import {
-  MorphIcon
+  MorphIcon,
+  PlusButton
 } from 'react-svg-buttons';
 import { TinderLikeCard } from 'react-stack-cards';
+import './Game.css'
 
-function CardStack(props) {
-    if (props.cards[props.value]){
-      let card = props.cards[props.value];
-      return <Card key={card.id} description={card.description} image_url={card.image_url}></Card>;
-    }else{
-      return <p>No more cards</p>;
-    }
-
-}
-
-class Game extends React.Component{
-  constructor(props){
+class Game extends React.Component {
+  constructor(props) {
     super(props)
-    this.state = {
-      value:0,
-      directionTinder: "swipeCornerDownRight",
-      isOpen: false
-    }
     this.Tinder = null
-  }
-  
-  onTinderSwipe() {
-    this.Tinder.swipe()
+    
   }
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: 0,
-  //   };
+  onTinderSwipe() {
+    this.Tinder.swipe();
+  }
+
+  // handleClick() {
+  //   this.forceUpdate();
+  //   return(
+  //     <div className="Game">
+  //     <PlusButton />
+  //   </div>
+  //   );
   // }
 
-   render(){
-     const images = this.props.cards.map(card => card.image_url);
-     const descriptions = this.props.cards.map(card => card.description);
-     return(
-       <div className="Game">
-       <MorphIcon
-          className="morph"
-          type="arrowLeft"
-          size={75}
-          thickness={1}
-          color="#202020"
-          onClick={() => this.setState({value: this.state.value - 1})}
-        />
-        <div>
-          <CardStack cards={this.props.cards} value={this.state.value}></CardStack>
-        </div>
-        <MorphIcon
-          className="morph"
-          type="arrowRight"
-          size={75}
-          thickness={1}
-          color="#202020"
-          onClick={() => this.setState({value: this.state.value + 1})}
-        />
-        {/* <div>
-        <TinderLikeCard
-            images={images}
-            width="350"
-            height="250"
-            direction={this.state.directionTinder}
-            duration={400}
+  render() {
+    const images = this.props.cards.map(card => "#202020");
+    const last_id = this.props.cards.length - 1;
+    const first_card = this.props.cards[0];
+
+      return (
+        <div className="Game">
+          <TinderLikeCard
+            colors={images}
+            width="400"
+            height="450"
+            direction="swipeCornerDownRight"
+            duration={100}
             ref={(node) => this.Tinder = node}
             className="tinder">
-            {descriptions.map(card => <div>{card}</div>)}
-        </TinderLikeCard>
+            {this.props.cards.map(card =>
+              <Card key={card.id}
+              description={card.description}
+              image_url={card.image_url}
+              last_id={last_id}
+              value={card.id-first_card.id}
+              // onTest={() => this.handleClick()}
+              />
+            )}
+          </TinderLikeCard>
+          <button onClick={this.onTinderSwipe.bind(this)}>
+            <MorphIcon
+              type="check"
+              size={75}
+              thickness={2}
+              color="#202020" />
+          </button>
         </div>
-        <button  onClick={this.onTinderSwipe.bind(this)}>
-        <MorphIcon
-          type="check" 
-          size={75}
-          thickness={1}
-          color="#202020"
-        /></button> */}
+  
+      );    
 
-       </div>
+  }
 
-     );
-
-   }
-   
 }
 
 export default Game;
