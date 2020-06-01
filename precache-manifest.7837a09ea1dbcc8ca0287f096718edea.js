b156cfa7f17d728d449bf8045dcfaf9eb1bb1bbb
sracela <saracelalfonso@gmail.com>
2020-06-01 21:52:40 +0200

    Updates

---
+++ precache-manifest.7837a09ea1dbcc8ca0287f096718edea.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c6a24b995551ee3d4384bbd42779d4",
+    "url": "/quarantini-webapp/index.html"
+  },
+  {
+    "revision": "da2856cffa292a6ccdbb",
+    "url": "/quarantini-webapp/static/css/main.c0698a93.chunk.css"
+  },
+  {
+    "revision": "ca70bb4216735c890e8f",
+    "url": "/quarantini-webapp/static/js/2.4d2527ce.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/quarantini-webapp/static/js/2.4d2527ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2856cffa292a6ccdbb",
+    "url": "/quarantini-webapp/static/js/main.85292aee.chunk.js"
+  },
+  {
+    "revision": "2e8a3d54302f9ab0c3cd",
+    "url": "/quarantini-webapp/static/js/runtime-main.4493483c.js"
+  },
+  {
+    "revision": "f33a2f8df343fdcf2a604886f833c1f4",
+    "url": "/quarantini-webapp/static/media/1howto.f33a2f8d.jpg"
+  },
+  {
+    "revision": "5a12f0961dac1d7e0937aa4014afa9a2",
+    "url": "/quarantini-webapp/static/media/2howto.5a12f096.jpg"
+  },
+  {
+    "revision": "fd61da3245b1171091ed9fd1e5618902",
+    "url": "/quarantini-webapp/static/media/3howto.fd61da32.jpg"
+  },
+  {
+    "revision": "d26653e3b6c4509fffcd78e3ab728234",
+    "url": "/quarantini-webapp/static/media/covid1.d26653e3.jpg"
+  },
+  {
+    "revision": "186119044903616694b33b155214aa6b",
+    "url": "/quarantini-webapp/static/media/quarantini_logo.18611904.jpg"
+  },
+  {
+    "revision": "6bb4245c11bd52d2a41514b8776ad2d3",
+    "url": "/quarantini-webapp/static/media/sara.6bb4245c.jpeg"
+  },
+  {
+    "revision": "41a05dbdb0b3b80a5b72fa01646e2563",
+    "url": "/quarantini-webapp/static/media/vic.41a05dbd.jpeg"
+  }
+]);
