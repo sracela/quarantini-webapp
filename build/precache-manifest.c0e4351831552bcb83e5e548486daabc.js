dd96e70a565e5f96571709063f82d5caa22c2b68
sracela <saracelalfonso@gmail.com>
2020-06-27 19:47:40 +0200

    Add build

---
+++ build/precache-manifest.c0e4351831552bcb83e5e548486daabc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac12d67ee3aa9d5d7f384c5ddae3f564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55de58d3379e6dcd8f1f",
+    "url": "/static/css/main.d565183c.chunk.css"
+  },
+  {
+    "revision": "4fb4db7be43c508732dc",
+    "url": "/static/js/2.01737b90.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/static/js/2.01737b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55de58d3379e6dcd8f1f",
+    "url": "/static/js/main.e0153ded.chunk.js"
+  },
+  {
+    "revision": "3cef7b8765452d630a09",
+    "url": "/static/js/runtime-main.5dc100b8.js"
+  },
+  {
+    "revision": "f33a2f8df343fdcf2a604886f833c1f4",
+    "url": "/static/media/1howto.f33a2f8d.jpg"
+  },
+  {
+    "revision": "5a12f0961dac1d7e0937aa4014afa9a2",
+    "url": "/static/media/2howto.5a12f096.jpg"
+  },
+  {
+    "revision": "fd61da3245b1171091ed9fd1e5618902",
+    "url": "/static/media/3howto.fd61da32.jpg"
+  },
+  {
+    "revision": "d26653e3b6c4509fffcd78e3ab728234",
+    "url": "/static/media/covid1.d26653e3.jpg"
+  },
+  {
+    "revision": "186119044903616694b33b155214aa6b",
+    "url": "/static/media/quarantini_logo.18611904.jpg"
+  },
+  {
+    "revision": "6bb4245c11bd52d2a41514b8776ad2d3",
+    "url": "/static/media/sara.6bb4245c.jpeg"
+  },
+  {
+    "revision": "41a05dbdb0b3b80a5b72fa01646e2563",
+    "url": "/static/media/vic.41a05dbd.jpeg"
+  }
+]);
